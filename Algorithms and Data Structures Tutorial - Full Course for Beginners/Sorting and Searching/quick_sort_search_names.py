431490b62ece3bd3e1bf0782cdd5666deb7d902a
Miroslav <mirovatov@gmail.com>
2021-10-12 16:45:46 +0300

    Add files via upload

---
+++ Algorithms and Data Structures Tutorial - Full Course for Beginners/Sorting and Searching/quick_sort_search_names.py
@@ -0,0 +1,15 @@
+from quick_sort import quicksort
+from load import load_strings
+
+
+unsorted_names = load_strings('names/unsorted.txt')
+sorted_names = quicksort(unsorted_names)
+
+file = open('names/sorted.txt', 'w')
+
+for n in sorted_names:
+    file.write(n + '\n')
+    print(n)
+
+
+file.close()
